89224848a7a15324a05d341abae2edcbea167c50
ChristianFish <cfishmusic@hotmail.com>
2022-06-07 09:36:41 -0500

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :title_is_clickbait
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :content, length: { minimum: 250 }
+
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Can't be True Facts")
+        end
+    end
 end
