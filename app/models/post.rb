081926fac4f0a7c576d156b0c662834f8e22e32a
ChristianFish <cfishmusic@hotmail.com>
2022-06-07 09:42:15 -0500

    commit tree a175abfe98270c570a76b0572933f2bb890f9b59

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,14 @@ class Post < ApplicationRecord
     validates :summary, length: { maximum: 250 }
     validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
     validates :content, length: { minimum: 250 }
+    
 
     def title_is_clickbait
         if title == "True Facts"
             errors.add(:title, "Can't be True Facts")
+        # elsif
+        #     title != ("Won't Believe", "Secret", "Top [number]", "Guess")
+        #     errors.add(:title, "Must include clickbait")
         end
     end
 end
